84b6a92b632361d5d602ebbdfc190547f0f46545
inesrienzo <irienzo.ieu2022@student.ie.edu>
2024-02-20 10:33:26 +0100

    eee

---
+++ read-from-file.py
@@ -0,0 +1,8 @@
+file_name = ("x-file.txt")
+fd = open(file_name)         # r is implicit
+
+# another way is to read it line by line
+print("here are the contents of the file:")
+print(fd.read())
+
+fd.close()
